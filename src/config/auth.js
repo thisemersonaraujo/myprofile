faa28f17d6a025330a18394726cc8c9f82a3ebd4
thisemersonaraujo <emerson.dev@outlook.com>
2021-10-30 17:23:38 -0300

    changing api secret password decoded

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 module.exports = {
-    secret: "ee1779775305ee0c7615e217e604b263cdc56c6cce934baf5502be4f34a849b9", //myapiprofile
+    secret: "ee1779775305ee0c7615e217e604b263cdc56c6cce934baf5502be4f34a849b9", //myprofileapi
     expiresIn: "6h"
 }
